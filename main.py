158c9fb5a6530f177311681e2f6b62643a0f1a6f
NA <NA@invalid.com>
2024-08-06 11:30:57 -0700

    concate send data

--- main.py
+++ main.py
@@ -7,17 +7,22 @@ def md5_top_4_bytes(input_string):
     md5_hash.update(input_string.encode('utf-8'))
     hash_bytes = md5_hash.digest()
     top_4_bytes = hash_bytes[:4]
-    return top_4_bytes[::-1]  # match c++ unsigned int storage
+    return top_4_bytes[::-1]
 
 def bytes_to_hex(byte_data):
-    # 将每个字节转换为 16 进制形式
     return [f"{byte:02x}" for byte in byte_data]
 
 def pack_rpc_header(magic_num, req_type, body_len, req_id, func_name):
     rpc_header_bytes = bytes([magic_num, req_type, 0xFF, 0xFF]) + struct.pack('I', body_len) + struct.pack('Q', req_id) + md5_top_4_bytes(func_name)
     return rpc_header_bytes
 
-
+def pack_data(raw_data):
+    packed_data = msgpack.packb(raw_data)
+    return packed_data
     
 if __name__ == "__main__":
-    print(bytes_to_hex(pack_rpc_header(39, 0, 6, 1, "echo")))
+    msg_bytes = pack_data("test")
+    header_bytes = pack_rpc_header(39, 0, len(msg_bytes), 1, "echo")
+    send_data = header_bytes + msg_bytes
+    print(bytes_to_hex(send_data))
+    
